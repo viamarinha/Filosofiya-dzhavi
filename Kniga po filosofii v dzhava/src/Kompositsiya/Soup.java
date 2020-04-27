516ecd94a10db392b9fbd5f41e0b5222803ba53a
viamarinha <63912351+viamarinha@users.noreply.github.com>
2020-04-27 09:41:49 -0400

    Sozadal noviy  klass soup

---
+++ Kniga po filosofii v dzhava/src/Kompositsiya/Soup.java
@@ -0,0 +1,16 @@
+package Kompositsiya;
+
+public class Soup {
+	private String s;
+
+	public Soup() {
+
+		System.out.println(" Soup constructed ");
+		s = "Borsh";
+	}
+	@Override
+	public String toString() {
+		return s;
+	}
+
+}
